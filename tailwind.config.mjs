d3afe66c8da97266478a4c852cec7abe2a9694fd
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-04-15 21:26:19 -0500

    style(project): :lipstick: add Tailwind styles and settings

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'cyan': 'hsl(180, 66%, 49%)',
+				'dark-violet': 'hsl(257, 27%, 26%)',
+				'red': 'hsl(0, 87%, 67%)',
+				'gray': 'hsl(0, 0%, 75%)',
+				'grayish-violet': 'hsl(257, 7%, 63%)',
+				'very-dark-blue': 'hsl(255, 11%, 22%)',
+				'very-dark-violet': 'hsl(260, 8%, 14%)',
+			},
+			backgroundColor: {
+				'cyan': 'hsl(180, 66%, 49%)',
+				'dark-violet': 'hsl(257, 27%, 26%)',
+				'red': 'hsl(0, 87%, 67%)',
+				'gray': 'hsl(0, 0%, 75%)',
+				'grayish-violet': 'hsl(257, 7%, 63%)',
+				'very-dark-blue': 'hsl(255, 11%, 22%)',
+				'very-dark-violet': 'hsl(260, 8%, 14%)',
+			},
+		},
+	},
+	plugins: [],
+}
