8bec14b0b29e8c6fd0545e519fab1cc70953d6e4
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-04-25 22:11:56 -0500

    style(project): :art: format

--- src/components/ShorteningSection.jsx
+++ src/components/ShorteningSection.jsx
@@ -15,10 +15,19 @@ const Form = () => {
   return (
     <form className="w-full bg-indigo-700 flex justify-between items-center gap-[25px] px-[66px] py-[50px] mt-[-84px] rounded-xl">
       <div className="flex-1 bg-black h-full px-[30px] py-[20px] text-[19px] font-medium rounded-xl">
-        <input type="text" placeholder="Shorten a link here..."  className="w-full bg-transparent"/>
+        <input
+          type="text"
+          placeholder="Shorten a link here..."
+          className="w-full bg-transparent"
+        />
         <label htmlFor=""></label>
       </div>
-      <button type="button" className="bg-red py-[15px] px-[40px] text-[20px] font-bold rounded-xl">Shorten It!</button>
+      <button
+        type="button"
+        className="bg-red py-[15px] px-[40px] text-[20px] font-bold rounded-xl"
+      >
+        Shorten It!
+      </button>
     </form>
   );
 };
