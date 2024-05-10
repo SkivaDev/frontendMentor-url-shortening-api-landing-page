b1a7afac6700bf53e1d1fd52d26f6d68bbf7f1e6
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-10 09:34:02 -0500

    style(FormSection): :lipstick: add mobile design to form

--- src/components/ShorteningSection.jsx
+++ src/components/ShorteningSection.jsx
@@ -13,18 +13,18 @@ const ShorteningSection = () => {
 
 const Form = () => {
   return (
-    <form className="w-full bg-indigo-700 flex justify-between items-center gap-[25px] px-[66px] py-[50px] mt-[-84px] rounded-xl">
-      <div className="flex-1 bg-black h-full px-[30px] pt-[20px] pb-[17px] text-[19px] font-medium rounded-[.4375rem]">
+    <form className="w-full bg-indigo-700 flex flex-col md:flex-row justify-between items-center gap-[16px] md:gap-[25px] px-[24px] md:px-[66px] py-[25px] md:py-[50px] mt-[-80px] md:mt-[-84px] rounded-xl">
+      <div className="w-full md:flex-1 bg-white h-full px-[16px] md:px-[30px] py-[10px] md:pt-[20px] md:pb-[17px] text-[19px] font-medium rounded-[7px] tracking-[0.5px] md:tracking-normal">
         <input
           type="text"
           placeholder="Shorten a link here..."
-          className="w-full bg-transparent text-[20px]"
+          className="w-full bg-transparent text-[15px] md:text-[20px]"
         />
         <label htmlFor=""></label>
       </div>
       <button
         type="button"
-        className="bg-red py-[15px] px-[40px] text-[20px] font-bold rounded-[.4375rem]"
+        className="w-full md:w-auto bg-cyan text-white py-[8px] md:py-[15px] px-[40px] text-[20px] font-bold rounded-[.4375rem] tracking-[-0.99px] md:tracking-normal"
       >
         Shorten It!
       </button>
